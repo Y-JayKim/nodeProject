817fb0b8183a61d3d6286875c8e35b84a6454941
Jay Kim <yeonjae.jay.kim@gmail.com>
2018-04-06 01:21:51 -0700

    Testing Deployment

--- server.js
+++ server.js
@@ -11,33 +11,9 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.use((request, response, next) => {
-	var time = new Date().toString();
-	// console.log(`${time}: ${request.method} ${request.url}`);
-	var log = `${time}: ${request.method} ${request.url}`;
-	fs.appendFile('server.log', log + '\n', (error)=>{
-		if (error) {
-			console.log('Unable to log message');
-		}
-	});
-	next();
+app.get('/',(request, response) =>{
+	response.render('main.hbs', {root: __dirname })
 });
-
-app.get('/', (request, response) => {
-	// response.send('<h1>Hello Express</h1>');
-	response.send({
-		name: 'Your Name',
-		school: [
-			'BCIT',
-			'SFU',
-			'UBC'
-		]
-	})
-});
-
-// app.get('/',(request, response) =>{
-// 	response.render('main.hbs', {root: __dirname })
-// });
 
 app.get('/signin', (request, response) => {
 	response.render('signin.hbs', {root: __dirname })
